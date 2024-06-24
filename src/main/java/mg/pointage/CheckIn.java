8430cdf3a6c5dfdeca013811df881b968760d743
ManoaLahatra <manoalahatra07@gmail.com>
2024-06-24 11:15:22 +0300

    feat: CheckIn

---
+++ src/main/java/mg/pointage/CheckIn.java
@@ -0,0 +1,14 @@
+package mg.pointage;
+
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+public class CheckIn {
+    private final Calendar calendar;
+
+    public DayTypes getDayOfCalendar(LocalDate date) {
+        return calendar.getDays().get(date);
+    }
+}
